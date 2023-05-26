e47ccee2421edf0980220e1560d412c6c45e0aae
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-26 15:55:32 -0300

    Loop de conjuntos

---
+++ Conjuntos - Python/Loop de conjuntos.py
@@ -0,0 +1,17 @@
+### Conjuntos ###
+
+fruitSet = {"maçã", "pêra", "laranja", "cereja"}
+carsSet = {"tesla", "honda", "volvo", "fiat"}
+nameSet = {"Théo", "Larissa", "Alan", "Beatriz", "Lívia"}
+
+print("--------------------------------------------------------- \n")
+
+print("\t Conjuntos: \n")
+print(fruitSet, "\n")
+print(carsSet, "\n")
+print(nameSet, "\n")
+
+print("--------------------------------------------------------- \n")
+
+for x in fruitSet:
+  print(x, "\n")
