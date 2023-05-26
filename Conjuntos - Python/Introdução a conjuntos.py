d2b7a114572424a112f498abcc987e5ca13631e1
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-26 15:47:30 -0300

    Introdução a conjuntos

---
+++ Conjuntos - Python/Introdução a conjuntos.py
@@ -0,0 +1,29 @@
+### Tuplas ###
+
+carsTuple = ("honda", "tesla", "volvo", "fiat")
+fruitTuple = ("maçã", "laranja", "abacate", "cereja")
+peopleTuple = ("Alan", "Théo", "Larissa", "Lívia", "Beatriz")
+
+print("-------------------------------------------------------------\n")
+
+print("TUPLAS: \n")
+
+print(carsTuple, "\n")
+print(fruitTuple, "\n")
+print(peopleTuple, "\n")
+
+print("-------------------------------------------------------------\n")
+
+### Juntar duas Tupals ###
+
+print("\t Juntar duas Tuplas: \n")
+
+mergeTuple = carsTuple + peopleTuple
+print(mergeTuple, "\n")
+
+### Multiplicar Tuplas: ###
+
+print("\t Multiplicar Tupals: \n")
+
+tupleTimes2 = carsTuple * 2
+print(tupleTimes2, "\n")
