85ebb7023e011bf4133da9850fc227233577aec9
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-26 13:20:17 -0300

    copy lists

---
+++ LIistas - Python/Copiar listas.py
@@ -0,0 +1,26 @@
+### Listas ###
+
+randomList = [55, "Théo", "Larissa", "Alan", 87, "Lívia", "Beatriz"]
+listNum = [1, 2, 3, 4, 5]
+
+print("-------------------------------------------------------------- \n")
+print("\t Listas: \n")
+print(randomList)
+print("\n")
+print(listNum)
+print("\n")
+print("-------------------------------------------------------------- \n")
+
+### Copiar Listas ###
+
+print("\t Copiando uma lista usando o 'copy()': \n")
+copyRandomList = randomList.copy()
+print(copyRandomList)
+print("\n")
+
+print("\t Copiando uma lista usando o 'list()': \n")
+copyListNum = list(listNum)
+print(copyListNum)
+print("\n")
+
+print("-------------------------------------------------------------- \n")
