38495d29249ac1d96416882850c0229189a3b467
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-29 13:48:53 -0300

    Variáveis

---
+++ Variáveis - Python/Atribuir vários valores.py
@@ -0,0 +1,31 @@
+print(" - Atribuir vários valores: \n")
+
+print("Atribuir valores para várias vvariáveis em uma só linha:", "\n")
+
+fruit1, fruit2, fruit3 = "Laranja", "Maçã", "Abacaxi"
+
+print(fruit1)
+print(fruit2)
+print(fruit3, "\n")
+
+###
+
+(" - Atribuir um único valor para várias variáveis: \n")
+
+x = y = z = 100
+
+print(x)
+print(y)
+print(z, "\n")
+
+###
+
+print(" - Desempacotar uma lista, tupla etc: \n")
+
+names = ["Alan", "Théo", "Larissa"]
+
+a, b, c = names
+
+print(a)
+print(b)
+print(c, "\n")
