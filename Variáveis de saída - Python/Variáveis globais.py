ec539f2c42876e3d9cfcaaefbde3a8717867fdeb
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-29 15:32:49 -0300

    Variáveis

---
+++ Variáveis de saída - Python/Variáveis globais.py
@@ -0,0 +1,6 @@
+print(" - Variáveis globais: \n")
+
+# Variáveis ​​criadas fora de uma função são conhecidas como variáveis ​​globais.
+
+#As variáveis ​​globais podem ser usadas por todos, tanto dentro quanto fora das funções.
+
