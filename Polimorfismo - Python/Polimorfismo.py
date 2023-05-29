2520bf565ac899b36c7a054ad7d4966c3c21158d
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-29 12:46:38 -0300

    Polimorfismo

---
+++ Polimorfismo - Python/Polimorfismo.py
@@ -0,0 +1,22 @@
+# Polimofirsmo #
+
+# A palavra "polimorfismo" significa "muitas formas", e na programação refere-se a métodos/funções/operadores #2 com o mesmo nome que podem ser executados em muitos objetos ou classes. #
+
+print(" - Polimorfismo: \n")
+
+# a função 'len()' pode ser usada em diferentes objetos
+
+print("Função 'len()' para string: \n")
+
+x = "Olá Mundo!"
+print(len(x),"\n")
+
+#
+
+print("Função 'len()' para tupla: \n")
+
+fruitTuple = ("maçã", "banana", "cereja")
+print(len(fruitTuple), "\n")
+
+#
+
