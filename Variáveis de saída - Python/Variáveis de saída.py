16cf890f1553766c1b08d4bd24f74c8136c9c370
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-29 15:32:59 -0300

    VAriáveis de saída

---
+++ Variáveis de saída - Python/Variáveis de saída.py
@@ -0,0 +1,19 @@
+print(" - Variáveis de saída: \n")
+
+print("A função 'print()' você gera variáveis por uma vírgula ou um '+': \n")
+
+a = 'Python'
+b = 'é'
+c = 'legal'
+
+print(a, b, c, ".\n")
+print(a + b + c + "\n") # Imprimirá tudo junto pois não a um espaço quando as strings são declaradas
+
+### 
+
+print("O operador '+' também serve como operador matemático: \n")
+
+num1 = 5
+num2 = 10
+
+print(num1 + num2, "\n")
