570119cefe9a95b55d5785f4b3b2119aebaaa957
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-31 14:02:19 -0300

    add contents for string folder

---
+++ Strings - Python/Strings.py
@@ -0,0 +1,33 @@
+print("\t - Strigs: \n")
+
+# Strings são 'Arrays' (Matrizes)
+
+print(" - Obter um caracter em uma string: \n")
+
+a = "Théo"
+print(a[2], "\n")
+
+### 
+
+print(" - Percorrer ma string, utilizando um 'for': \n")
+
+for x in "BANANA":
+    print(x)
+
+print("\n")
+
+###
+
+print(" - Obter o comprimento de uma string, utilizanod a função 'len()': \n")
+
+word1 = "Hello world!"
+print(len(word1), "\n")
+
+###
+
+print(" - Verificar se uma frase ou caractere está presente em uma string: \n") # Usando o 'in'
+
+txt = "Vivendo o meu melhor momento."
+
+print("momento" in txt)
+
