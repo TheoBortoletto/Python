ff1dfe6b6111f552a11e74d5be6750aff6a25daf
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-31 15:46:54 -0300

    add content

---
+++ Strings - Python/Cortar  strings.py
@@ -0,0 +1,11 @@
+print("\t - Cortar Strings: \n")
+
+print(" - Imprimindo um interavlo de string: \n")
+
+txt = "Olá Mundo!!"
+print(txt[4:9]) # a posição 9 seria o '!', mas ela não esta incluida, seria necessário trocar o 9 por 10
+
+print(" - Imprimir o início de uma string até onde for desejado: \n")
+
+txt2 = "Homem-Aranha"
+print(txt2[:5], "\n")
