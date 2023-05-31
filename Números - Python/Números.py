863c4cab992375600797a514496c9ba0b9994de7
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-05-31 12:08:57 -0300

    add content

---
+++ Números - Python/Números.py
@@ -0,0 +1,21 @@
+print("\t - Números do Python: \n")
+
+# Existem 3 tipos numéricos no Python: int, floaat, complex
+
+print("Exemplos: \n")
+
+x = 10 # int
+y = 15.55 # float
+z = 5j # complex
+
+print(x, "= int \n")
+print(y, "= float \n")
+print(z, "= complex \n")
+
+### 
+
+print(" - Verificar o tipo de dado numérico, utilize a função 'type()': \n")
+
+print(type(x))
+print(type(y))
+print(type(z), "\n")
