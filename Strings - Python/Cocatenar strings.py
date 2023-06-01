93493e3a4889be9c209ff5d92be8b464cef246fc
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-01 15:43:02 -0300

    add content

---
+++ Strings - Python/Cocatenar strings.py
@@ -0,0 +1,20 @@
+print("\t - Cocatenar strings: \n")
+
+a = "Exemplo"
+b = " de "
+c = "concatenação"
+d = " de "
+e = "string"
+
+fullTxt = a + b + c + d + e
+
+print(fullTxt, "\n")
+
+### 
+
+txt = "Outro"
+txt2 = "exemplo"
+
+allTxt = txt + " " + txt2
+
+print(allTxt, "\n")
