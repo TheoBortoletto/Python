5f5fce6d6168abf9018671af479c25dc41108412
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-01 15:38:20 -0300

    add modificar strings

---
+++ Strings - Python/Modificar strings.py
@@ -0,0 +1,41 @@
+print("\t - Modificar strings: \n")
+
+print(" - Imprimir texto em CapsLock \n")
+
+upperTxt = "Esse texto está em CapsLock"
+
+print(upperTxt.upper(), "\n")
+
+### 
+
+print(" - Imprimir tudo em minúsculo: \n")
+
+lowerTxt = "Esse texto está tudo em minúsculo"
+
+print(lowerTxt.lower(), "\n")
+
+### 
+
+print(" - Removendo espaços em branco de uma string: \n")
+
+stripTxt = "  Removeu os espaços em branco no começo e no final  "
+
+print(stripTxt.strip(), "\n")
+
+###
+
+print(" - Substituir string por outra: \n")
+
+replaceTxt = "Olá Mundo!!!"
+
+print(replaceTxt.replace("Mundo", "a Todos"), "\n")
+
+### 
+
+print(" - Dividir string: \n")
+
+splitTxt = "Dividir, string"
+
+txt = splitTxt.split(",")
+
+print(txt, "\n")
