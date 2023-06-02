d993abfbd10a2fb8918f5311432adb665d43b61e
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-02 10:27:21 -0300

    add  content

--- Booleanos - Python/Booleanos.py
+++ Booleanos - Python/Booleanos.py
@@ -1,3 +1,32 @@
 print("\t - Booleanos: \n")
 
 # Booleanos representam dois valores: 'True' ou 'False'
+
+print(" - Exemplo de expressão booleana, checar se é veredadeiro ou falso com um 'if': \n")
+
+a = 200
+b = 199
+
+if b > a:
+    print(b, "é maior do que", a)
+
+else:
+    print(a, "é maior que", b, "\n")
+
+### 
+
+print(" - Avaliar valores e variáveis, retornando True ou Flase: \n")
+
+print(bool("Hello"))
+print(bool(0), "\n")
+
+txt = "Hello"
+txt2 = ""
+
+print(bool(txt))
+print(bool(txt2), "\n")
+
+###
+
+
+
