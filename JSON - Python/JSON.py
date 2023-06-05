bf637c61d5f88460bcd81b8dab15a68b19f8005e
THÉO HENRIQUE GHETTI BORTOLETTO <theo.bortoletto@gmail.com>
2023-06-05 11:01:29 -0300

    add content

--- JSON - Python/JSON.py
+++ JSON - Python/JSON.py
@@ -1,4 +1,9 @@
 print("\t - JSON Python: \n")
 
+### O Python possui um pacote interno chamado json, 
+# que pode ser usado para trabalhar com dados JSON.
+
+print(" - Converter JSON pra Python: \n")
+
 import json
 
